0957c435dfe2456b679f49cad73c57cb1c64be07
shom45 <150340519+shom45@users.noreply.github.com>
2023-11-09 23:56:29 +0600

    Update Task_101.m

--- Task_101.m
+++ Task_101.m
@@ -1,3 +1,15 @@
-A=[1 2 0;4 5 8;1 8 9]
+function sowmik = task_101 ()
 
-a=max(max(A))
+   V= input("Matrix V : ");
+   C= input("Matrix C : ");
+   mx_V = max(max(V));
+   fprintf("Maximum value of V : %d\n", mx_V);
+   if size(V,2)==size(C,1)
+      Mul = V * C;
+      fprintf("Multiplication :\n");
+      disp (Mul);
+   else
+      fprintf ("Matrix multiplication is not possible!\n");
+   end
+
+endfunction
